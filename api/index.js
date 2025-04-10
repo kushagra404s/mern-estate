5786d1a4eda3418da44c61974ea115db03e94d17
Kushagra Singh <iamkushagra2021@gmail.com>
2025-04-11 01:23:42 +0530

    created signup api routes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 const app=express();
@@ -14,8 +15,10 @@ mongoose.connect(process.env.MONGO)
     console.log('Error connecting to MongoDB:', err);
   });
 
+  app.use(express.json());
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
 
-app.use("/api/user",userRouter)
+app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
