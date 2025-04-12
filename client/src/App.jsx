170adaf53dcb4c8c51a6c3ffa5daabf6d8dcb415
Kushagra Singh <iamkushagra2021@gmail.com>
2025-04-12 21:48:58 +0530

    Sign-In page completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/signin" element={<Signin />} />
-        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
      </Routes>
