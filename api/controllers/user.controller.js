b0b34675299400cb7f37c0cbd6e8ba2bfebc1124
Kushagra Singh <iamkushagra2021@gmail.com>
2025-06-17 09:42:20 +0530

    profile picture update

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import { errorHandler } from '../utils/error.js';
 export const test=(req, res) => {
     res.json({
         message: "Hello from user controller"
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,23 @@
+import { app } from '../firebase';
 import {useSelector} from "react-redux";
-import { useState, useRef } from "react";
-
+import { useState, useRef, useEffect } from "react";
+import {getDownloadURL, uploadBytesResumable ,getStorage,ref } from "firebase/storage";
 import { updateUserStart,updateUserSuccess,updateUserFailure } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+
 export default function Profile() {
   const fileRef=useRef(null);
   const {currentUser,loading,error} = useSelector((state) => state.user);
   const dispatch = useDispatch();
+  
+  const[file, setFile] = useState(undefined);
+  const[filePerc,setFilePerc] = useState(0);
+  const[fileUploadError,setFileUploadError] = useState(false);
+  
 
   const[formData, setFormData] = useState({})
   const [updateSuccesss,setUpdateSuccess] = useState(false);
+
 
   const handleChange = (e) => {
     setFormData({
@@ -30,7 +38,7 @@ export default function Profile() {
           },
           body: JSON.stringify(formData)
         });
-          
+          console.log("Submitting formData:", formData);
         const data = await res.json();
         if(data.success===false) {
           dispatch(updateUserFailure(data.message));
@@ -47,16 +55,68 @@ export default function Profile() {
 
   }
 
+  useEffect(()=>{
+    if(file){
+      handleFileUpload(file);
+    }
+  },[file]);
+
+  const handleFileUpload = async (file) => {
+    const storage=getStorage(app);
+    
+
+    const fileName=new Date().getTime()+file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+     (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+  };
+
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input type="file" ref={fileRef} hidden accept="image/*" />
+         <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type='file'
+          ref={fileRef}
+          hidden
+          accept='image/*'
+        />
 <img onClick={()=> fileRef.current.click()}
-  src={currentUser?.avatar || "https://via.placeholder.com/150"}
+  src={formData.avatar || currentUser.avatar ||'https://th.bing.com/th/id/OIP.ATZnbS6I6DVPWuFGum_U0AHaHa?w=204&h=204&c=7&r=0&o=7&dpr=2.1&pid=1.7&rm=3'}
   alt="profile"
-  className="rounded-full h-24 w-24 object-cover cursor-pointer self-center"
+  className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
 />
+
+<p className='text-sm self-center'>
+          {fileUploadError ? (
+            <span className='text-red-700'>
+              Error Image upload (image must be less than 2 mb)
+            </span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className='text-green-700'>Image successfully uploaded!</span>
+          ) : (
+            ''
+          )}
+        </p>
 
 <input type="text" placeholder="username" defaultValue={currentUser.username} id="username" className="border p-3 rounded-lg" onChange={handleChange} />
 
