c4a905aba74efe36788a98fec268d087db6b92b4
Kushagra Singh <iamkushagra2021@gmail.com>
2025-08-01 18:38:08 +0530

    linked search bar with url and vice versa

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,29 @@
 import {FaSearch} from 'react-icons/fa';
-import{Link} from 'react-router-dom';
+import{Link,useNavigate} from 'react-router-dom';
 import{useSelector} from 'react-redux';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 
 export default function Header() {
   const {currentUser} = useSelector(state => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+ const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+   const urlParams=new URLSearchParams(window.location.search);
+   urlParams.set('searchTerm',searchTerm);
+   const searchQuery=urlParams.toString();
+   navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(()=>{
+   const urlParams=new URLSearchParams(location.search);
+   const searchTermFromUrl=urlParams.Params.get('searchTerm');
+   if(searchTermFromUrl) {
+    setSearchTerm(searchTermFromUrl);
+   }
+  })
   return (
     <header className='bg-slate-200 shadow-md '>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -15,9 +34,12 @@ export default function Header() {
         </h1>
               </Link>
 
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-            <input type="text" placeholder='search..' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-            <FaSearch className='text-slate-600'/>
+        <form  onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
+            <input type="text" placeholder='search..' className='bg-transparent focus:outline-none w-24 sm:w-64'
+            onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button> <FaSearch className='text-slate-600'/></button>
+           
         </form>
           
           <ul className='flex gap-4'>
