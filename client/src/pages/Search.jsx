75165a74f6e0508da36fafb4e523cddb6effb151
Kushagra Singh <iamkushagra2021@gmail.com>
2025-08-02 19:03:26 +0530

    Implement search functionality with URL parameter handling and form submission

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,17 +1,88 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {useNavigate} from 'react-router-dom'
 
 export default function Search() {
+
+  const[sidebardata,setSidebardata]=useState({
+    searchTerm:'',
+    type:'all',
+    parking:false,
+    furnished:false,
+    offer:false,
+    sort:'created_at',
+    order:'desc',
+  })
+
+  useEffect(()=>{
+    const urlParams=new URLSearchParams(location.search);
+    const searchTermFromUrl=urlParams.get('searchTerm') 
+    const typeFromUrl=urlParams.get('type') 
+    const parkingFromUrl=urlParams.get('parking')
+    const furnishedFromUrl=urlParams.get('furnished')
+    const offerFromUrl=urlParams.get('offer') 
+    const sortFromUrl=urlParams.get('sort') 
+    const orderFromUrl=urlParams.get('order')
+    
+    if(searchTermFromUrl|| typeFromUrl || parkingFromUrl || furnishedFromUrl || offerFromUrl || sortFromUrl || orderFromUrl){
+      setSidebardata({
+        searchTerm: searchTermFromUrl || '',
+        type: typeFromUrl || 'all',
+        parking: parkingFromUrl === 'true'?true: false,
+        furnished: furnishedFromUrl === 'true' ?true: false,
+        offer: offerFromUrl === 'true' ?true: false,
+        sort: sortFromUrl || 'created_at',
+        order: orderFromUrl || 'desc',
+      },[location.search]);
+    }
+  })
+
+  const handleChange=(e)=>{
+    if(e.target.id==='all'|| e.target.id==='rent' || e.target.id==='sale'){
+      setSidebardata({...sidebardata, type: e.target.id})
+    }
+    if(e.target.id==='searchTerm'){
+      setSidebardata({...sidebardata, searchTerm: e.target.value})
+    }
+    if(e.target.id==='parking'|| e.target.id==='furnished' || e.target.id==='offer'){
+      setSidebardata({...sidebardata, [e.target.id]: e.target.checked || e.target.checked==='true' ? true : false})
+    }
+
+    if(e.target.id==='sort_order'){
+      const sort=e.target.value.split('_')[0] || 'created_at';
+      const order=e.target.value.split('_')[1] || 'desc';
+      setSidebardata({...sidebardata, sort, order})
+    }
+      
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const urlParams=new URLSearchParams();
+    urlParams.set('searchTerm', sidebardata.searchTerm);
+    urlParams.set('type', sidebardata.type);
+    urlParams.set('parking', sidebardata.parking);
+    urlParams.set('furnished', sidebardata.furnished);
+    urlParams.set('offer', sidebardata.offer);
+    urlParams.set('sort', sidebardata.sort);
+    urlParams.set('order', sidebardata.order);
+
+    const searchQuery=urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+
+  }
+
   return (
     <div className='flex flex-col md:flex-row'>
         
         <div className='p-7 border-b-2 m:border-r-2 md:min-h-screen'>
-       <form className='flex flex-col gap-8'>
+       <form onSubmit={handleSubmit} className='flex flex-col gap-8'>
         <div className="flex items-center gap-2" >
             <label className='whitespace-nowrap'> search Term:</label>
             <input type="text"
             id='searchTerm'
             placeholder='search..'
             className=' border rounded-lg p-3 w-full m-1'
+            onChange={handleChange}
             ></input>
         </div>
 
@@ -21,6 +92,8 @@ export default function Search() {
              <div className='flex gap-2'>
               <input type="checkbox" id="all" 
               className='w-5'
+              onChange={handleChange}
+              checked={sidebardata.type === 'all'}
               />
               <span> Rent & Sell</span>
              </div>
@@ -28,6 +101,8 @@ export default function Search() {
              <div className='flex gap-2'>
               <input type="checkbox" id="rent" 
               className='w-5'
+              onChange={handleChange}
+              checked={sidebardata.type === 'rent'}
               />
               <span> Rent </span>
              </div>
@@ -35,6 +110,8 @@ export default function Search() {
              <div className='flex gap-2'>
               <input type="checkbox" id="sale" 
               className='w-5'
+              onChange={handleChange}
+              checked={sidebardata.type === 'sale'}
               />
               <span> Sell</span>
              </div>
@@ -42,6 +119,8 @@ export default function Search() {
              <div className='flex gap-2'>
               <input type="checkbox" id="offer" 
               className='w-5'
+              onChange={handleChange}
+              checked={sidebardata.offer}
               />
               <span>Offer</span>
              </div>
@@ -53,6 +132,8 @@ export default function Search() {
              <div className='flex gap-2'>
               <input type="checkbox" id="parking" 
               className='w-5'
+              onChange={handleChange}
+              checked={sidebardata.parking}
               />
               <span> Parking</span>
              </div>
@@ -60,18 +141,23 @@ export default function Search() {
              <div className='flex gap-2'>
               <input type="checkbox" id="furnished" 
               className='w-5'
+              onChange={handleChange}
+              checked={sidebardata.furnished}
               />
               <span> Furnished </span>
              </div>
         </div>
 
-         <div classname= "flex items-center gap-2 ">
-          <lable>Sort:</lable>
-          <select className='border rounded-md p-1 m-1' id="sort_order">
-            <option>Price high to low</option>
-            <option>Price low to high</option>
-            <option>Oldest</option>
-            <option>Latest</option>
+         <div className= "flex items-center gap-2 ">
+          <label>Sort:</label>
+          <select onChange={handleChange}
+          defaultValue={'created_at_desc'}
+           className='border rounded-md p-1 m-1' id="sort_order">
+            <option value='regularPrice_desc'>Price high to low</option>
+          <option value='regularPrice_asc'>Price low to high</option>
+            <option value='createdAt_desc'>Latest</option>
+            <option value='createdAt_asc'>Oldest</option>
+            
           </select>
          </div>
              <button className="bg-slate-700 text-white p-2 rounded-lg uppercase hover:opacity-90"> Search</button>
