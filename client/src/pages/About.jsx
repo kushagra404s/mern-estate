1d43d84166863a029b72f562791a49220f27e4a3
Kushagra Singh <iamkushagra2021@gmail.com>
2025-08-03 06:56:57 +0530

    Update index.html title and enhance About page header; refactor Listing component imports

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+      <h1 className='text-3xl font-bold mb-4 flex justify-center'>About MarketPlace</h1>
+    </div>
   )
 }
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -5,19 +5,16 @@ import SwiperCore from 'swiper';
 import { useSelector } from 'react-redux';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
-// import {
-//   FaBath,
-//   FaBed,
-//   FaChair,
-//   FaMapMarkedAlt,
-//   FaMapMarkerAlt,
-//   FaParking,
-//   FaShare,
-// } from 'react-icons/fa';
-// import Contact from '../components/Contact';
-
-// https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
-
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkedAlt,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from 'react-icons/fa';
+import Contact from '../components/Contact';
 export default function Listing() {
   SwiperCore.use([Navigation]);
   const [listing, setListing] = useState(null);
